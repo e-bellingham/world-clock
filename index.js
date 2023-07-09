c8943c083aba08b77ae53bda57569eb7bc8613e5
Elizabeth <e.bellingham02@gmail.com>
2023-07-09 16:31:24 -0400

    changed html to work with moment.js time zones. Wrote JS code

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+setInterval(function () {
+  //New YORK
+  let nycElement = document.querySelector("#nyc");
+  let nycDateElement = nycElement.querySelector(".date");
+  let nycTimeElement = nycElement.querySelector(".time");
+  let nycTime = moment().tz("America/New_York");
 
+  nycDateElement.innerHTML = nycTime.format("LL");
+  nycTimeElement.innerHTML = nycTime.format("h:m:s [<small>]A[</small>]");
+}, 1000);
+
+setInterval(function () {
+  //Los Angeles
+  let laxElement = document.querySelector("#lax");
+  let laxDateElement = laxElement.querySelector(".date");
+  let laxTimeElement = laxElement.querySelector(".time");
+  let laxTime = moment().tz("America/Los_Angeles");
+
+  laxDateElement.innerHTML = laxTime.format("LL");
+  laxTimeElement.innerHTML = laxTime.format("h:m:s [<small>]A[</small>]");
+}, 1000);
